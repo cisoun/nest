3f522568e8a6ba349f3d381a501b4c57eb97561a
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2024-01-24 14:01:39 +0100

    Helpers: add `statics` to load static files

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,8 +1,18 @@
+const {readFile} = require('fs/promises');
+const {join}     = require('path');
+
 /**
  * Return current timestamp in seconds.
  */
 const now = () => Math.floor(Date.now() / 1000);
 
+/**
+ * Return the content of a static file.
+ * @param {String} path - Path of the static file.
+ */
+const statics = async (path) => await readFile(path, {encoding: 'utf-8'});
+
 module.exports = {
-	now
+	now,
+	statics
 }
