e264b67a461601224b88dce0676ec393b236fcf0
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2024-01-30 18:52:43 +0100

    HTML: allow default value when rendering parameter
    In HTML code, use `{{ var | defaultvalue }}`. To render the `var` parameter, or "defaultvalue" if `var` is undefined.

--- src/modules/html.js
+++ src/modules/html.js
@@ -5,18 +5,21 @@
 
 const FILE_PATTERN    = '\[\\w\\_\\.\]\+';
 const STATICS_PATTERN = `\{\{\\s*statics\\((${FILE_PATTERN})\\)\\s*\}\}`;
-const VAR_PATTERN     = /{{\s*(\w*)\s*(\|\s*(.*))?}}/g;
+const VAR_PATTERN     = /{{\s*(\w*)\s*(\|\s*(.*))?}}/;
 
 const statics_regex = new RegExp(STATICS_PATTERN, 'ig');
+const var_regex     = new RegExp(VAR_PATTERN, 'g');
 
 /**
  * Render an HTML template code.
  * @param {Object} params - Variables to add to the page.
  */
-function render (html, params=null) {
+function render (html, params={}) {
 	html = html.replace(statics_regex, (_, p1) => `statics/${p1}`);
 	if (params) {
-		return html.replace(/{{(\w*)}}/g, (_, p1) => params[p1] ?? '');
+		// IDEA: We could use JSON.stringify on the returned result to also add
+		//   quotes automatically.
+		return html.replace(var_regex, (_, p1, p2, p3) => params[p1] ?? p3 ?? '');
 	}
 	return html;
 }
