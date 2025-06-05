6652f9fe0ca1519dd0a815fb16c443d76c9f8107
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2025-06-05 21:40:02 +0200

    Helpers: fix `statics` + add `fileExtension` and `fileRead`

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,5 +1,11 @@
 const {readFile} = require('fs/promises');
-const {join}     = require('path');
+const {
+	extname,
+	join
+} = require('path');
+
+const fileExtension = extname;
+const fileRead = readFile;
 
 /**
  * Return current timestamp in seconds.
@@ -10,9 +16,11 @@ const now = () => Math.floor(Date.now() / 1000);
  * Return the content of a static file.
  * @param {String} path - Path of the static file.
  */
-const statics = async (path) => await readFile(path, {encoding: 'utf-8'});
+const statics = async (path) => await readFile(`statics/${path}`);
 
 module.exports = {
+	fileExtension,
+	fileRead,
 	now,
 	statics
 }
