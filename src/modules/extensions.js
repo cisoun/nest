a726f38c36094521dc0ba8643e8e5bcddd5ee2c9
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2025-06-05 21:42:24 +0200

    Extensions: add `response.file`

--- src/modules/extensions.js
+++ src/modules/extensions.js
@@ -39,15 +39,47 @@ const Request     = require('nest/requests');
 const Response    = require('nest/responses');
 const Server      = require('nest/server');
 const validation  = require('nest/validation');
-const {statics}   = require('nest/helpers');
-const {NestError} = require('nest/errors');
+const {HTTPError} = require('nest/errors');
+const {
+	fileExtension,
+	fileRead,
+	statics
+} = require('nest/helpers');
 
 const Extensions = {
 	'request.validate': request_validate,
+	'response.file':    response_file,
 	'response.html':    response_html,
 	'response.render':  response_render,
 	'statics':          statics_handler,
 };
+
+const MIMETypes = {
+	'jpg':  'image/jpeg',
+	'jpeg': 'image/jpeg',
+	'svg':  'image/svg+xml'
+};
+
+function response_file () {
+	Response.prototype.file = async function (path, params={}) {
+		return await fileRead(path)
+			.then(data => {
+				const buffer    = Buffer.from(data);
+				const extension = fileExtension(path).slice(1);
+				// Force content type for files known to not be recognized
+				// by the browser by default.
+				if (extension in MIMETypes) {
+					this.base.setHeader('Content-Type', MIMETypes[extension]);
+				}
+				this.base.setHeader('Content-Length', buffer.length);
+				this.base.write(buffer);
+				return this;
+			})
+			.catch(e => {
+				throw new HTTPError(404, 'cannot fetch file', {cause: path});
+			});
+	}
+}
 
 function response_html () {
 	Response.prototype.html = function (data, params={}) {
@@ -66,20 +98,18 @@ function request_validate () {
 function response_render (file, params={}) {
 	response_html();
 	Response.prototype.render = async function (file, params={}) {
-		const data = await statics(`statics/${file}.html`);
-		return this.html(data, params);
+		const data = await statics(file + '.html');
+		return this.html(data.toString(), params);
 	}
 }
 
 function statics_handler () {
+	response_file();
 	const f = Server.prototype.route;
 	Server.prototype.route = async function (req, res) {
 		const path = req.path;
 		if (req.method == 'GET' && path.startsWith('/statics')) {
-			const data = await statics(path.slice(1)).catch(e => {
-				throw new NestError(404, 'cannot fetch file');
-			});
-			res.base.write(Buffer.from(data));
+			return res.file(path.slice(1));
 		} else {
 			return f.call(this, req, res);
 		}
