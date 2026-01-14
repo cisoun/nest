ab3cc2f6c9f27e8d7e575a08f0febb2480c9e982
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2026-01-14 18:29:10 +0100

    Extensions: use new implementation
    - Extension registering is now removed.
    - Extensions must be implemented as (instance) => { ... }

--- src/extensions.js
+++ src/extensions.js
@@ -6,53 +6,69 @@
  *
  * Usage:
  *
+ *   const { requestGet, statics } = require('nest/extensions');
  *   const app = nest();
  *   app.use(
- *     'request.get',
- *     'statics'
+ *     requestGet,
+ *     statics
  *   );
  *
  * Extensions:
  *
- *   request.get:      Ensure a JSON body is given and get its specific keys.
- *                     E.g.: [name, age] = req.get('name', 'age');
+ *   apifs:           Build an API from the "api" folder.
  *
- *   request.id:       Add a UUID4 identifier to each request as
- *                     `id` attribute.
- *                     E.g.: log.info(req.id);
+ *                    Paths are translated as below:
+ *                      api/users/index.js -> /api/users
+ *                      api/users/[id].js  -> /api/users/<id>
  *
- *   request.validate: Validate the content of a request.
- *                     Expects an object or or a Validator instance.
- *                     E.g.: req.validate({'name': 'required'});
- *                     E.g.: req.validate(UserValidator);
+ *                    JS files must export functions named after a standard
+ *                    HTTP method (in upper case) in order to register them as
+ *                    route.
  *
- *   response.file:    Allow a response to send a file from a specific path.
- *                     E.g.: res.file('statics/images/logo.png');
+ *                    E.g.: exports.GET = (req, res) => res.text('hello');
  *
- *   response.html:    Allow a response to send HTML code.
- *                     E.g.: res.html('<div>Hello</div>');
+ *                    WARNING: the build process is asynchronous!
  *
- *   response.render:  Allow a response to render and serve an HTML static file.
- *                     Will load: response.html
- *                     E.g.: res.render('index', {version: '1.0'});
+ *   requestGet:      Ensure a JSON body is given and get its specific keys.
+ *                    E.g.: [name, age] = req.get('name', 'age');
  *
- *   statics:          Allow the server to serve static files through GET
- *                     requests with URLs starting by `/statics`.
+ *   requestId:       Add a UUID4 identifier to each request as `id` attribute.
+ *                    E.g.: log.info(req.id);
+ *
+ *   requestValidate: Validate the content of a request.
+ *                    Expects an object or or a Validator instance.
+ *                    E.g.: req.validate({'name': 'required'});
+ *                    E.g.: req.validate(UserValidator);
+ *
+ *   responseFile:    Allow a response to send a file from a specific path.
+ *                    E.g.: res.file('statics/images/logo.png');
+ *
+ *   responseHtml:    Allow a response to send HTML code.
+ *                    E.g.: res.html('<div>Hello</div>');
+ *
+ *   responseRender:  Allow a response to render and serve an HTML static file.
+ *                    Will load: responseHtml
+ *                    E.g.: res.render('index', {version: '1.0'});
+ *
+ *   statics:         Allow the server to serve static files through GET
+ *                    requests with URLs starting by `/statics`.
+ *                    Will load: responseFile
  */
 
-const HTML     = require('nest/html');
-const Request  = require('nest/requests');
-const Response = require('nest/responses');
+const Request    = require('nest/requests');
+const Response   = require('nest/responses');
+const helpers    = require('nest/helpers');
+const html       = require('nest/html');
+const { assert } = require('nest/assert');
+const { uuid4 }  = require('nest/crypto');
 const {
 	HTTPError,
 	HTTPValidationError,
-	NestError,
 	ValidationError
 } = require('nest/errors');
 const {
 	fileExtension,
 	fileRead,
-	statics
 } = require('nest/helpers');
 const {
 	validateKeys,
@@ -60,25 +76,71 @@ const {
 	Validator
 }  = require('nest/validation');
 
-
-const Extensions = {
-	'request.get':      request_get,
-	'request.id':       request_id,
-	'request.validate': request_validate,
-	'response.file':    response_file,
-	'response.html':    response_html,
-	'response.render':  response_render,
-	'statics':          statics_handler,
-};
-
 const MIMETypes = {
 	'jpg':  'image/jpeg',
 	'jpeg': 'image/jpeg',
 	'svg':  'image/svg+xml'
 };
 
-function request_get () {
+function use (instance, ...extensions) {
+	assert(extensions instanceof Array, 'no extensions given');
+	extensions.forEach((e) => {
+		assert(e instanceof Function, 'extension must be a function');
+		if (e.length == 1) {
+			e(instance);
+		} else if (e.length == 3) {
+			const old = instance.route;
+			instance.route = (req, res) => e(req, res, old);
+		}
+	});
+}
+
+const apifs = (instance) => {
+	const { join } = require('path');
+	const fs       = require('fs');
+
+	const cwd       = process.cwd();
+	const cwdLength = cwd.length;
+	const methods   = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'];
+	const options   = { recursive: true, withFileTypes: true };
+
+	/*
+	 * Here, we go recursively in /api to find each .js file, create a route
+	 * after their respective path and import their exports if they are named
+	 * after an HTTP method.
+	 *
+	 * Special case: index.js are not appended at the end of the path.
+	 *
+	 * WARNING: this part is asynchronous!
+	 */
+
+	fs.readdir(join(cwd, 'api'), options, (err, files) => {
+		files.forEach(file => {
+			const name = file.name;
+			if (file.isFile() && name.endsWith('.js')) {
+				const callbacks = require(join(file.parentPath, name));
+
+				let route = file.parentPath.slice(cwdLength);
+				if (name !== 'index.js') {
+					route = route + '/' + name.slice(0, name.lastIndexOf('.'));
+				}
+
+				methods.map((method) => {
+					const callback = callbacks[method];
+					if (callback) {
+						instance.register(method, route, callback);
+					}
+				});
+			}
+		});
+	});
+};
+
+const requestGet = (instance) => {
 	Request.prototype.get = function (...args) {
+		if (this.body.length < 1) {
+			throw new HTTPError(400, 'expected JSON payload');
+		}
 		try {
 			return validateKeys(this.json, args);
 		} catch (e) {
@@ -88,19 +150,17 @@ function request_get () {
 			throw e;
 		}
 	}
-}
+};
 
-function request_id (instance) {
-	const { uuid4 } = require('nest/crypto');
-	const f = instance.createRequest;
-	instance.createRequest = function (...args) {
-		const request = f(...args);
-		request.id = uuid4();
-		return request;
-	}
-}
+const requestId = (instance) => {
+	const requestId = async (req, res, next) => {
+		req['id'] = uuid4();
+		await next();
+	};
+	instance.use(requestId);
+};
 
-function request_validate () {
+const requestValidate = () => {
 	Request.prototype.validate = function (rules) {
 		try {
 			if (rules instanceof Validator) {
@@ -116,9 +176,9 @@ function request_validate () {
 			}
 		}
 	}
-}
+};
 
-function response_file () {
+const responseFile = (instance) => {
 	Response.prototype.file = async function (path) {
 		return await fileRead(path)
 			.then(data => {
@@ -127,67 +187,54 @@ function response_file () {
 				// Force content type for files known to not be recognized
 				// by the browser by default.
 				if (extension in MIMETypes) {
-					this.base.setHeader('Content-Type', MIMETypes[extension]);
+					this.setHeader('Content-Type', MIMETypes[extension]);
 				}
-				this.base.setHeader('Content-Length', buffer.length);
-				this.base.write(buffer);
+				this.setHeader('Content-Length', buffer.length);
+				this.write(buffer);
 				return this;
 			})
 			.catch(e => {
 				throw new HTTPError(404, 'cannot fetch file', {cause: path});
 			});
 	}
-}
+};
 
-function response_html () {
+const responseHtml = (instance) => {
 	Response.prototype.html = function (data, params = {}) {
-		this.base.setHeader('Content-Type', 'text/html; charset=utf-8');
-		this.base.write(HTML.render(data, params));
+		this.setHeader('Content-Type', 'text/html; charset=utf-8');
+		this.write(html.render(data, params));
 		return this;
 	}
-}
+};
 
-function response_render () {
-	response_html();
+const responseRender = (instance) => {
+	use(instance, responseHtml);
 	Response.prototype.render = async function (file, params={}) {
-		const data = await statics(file + '.html');
+		const data = await helpers.statics(file + '.html');
 		return this.html(data.toString(), params);
 	}
-}
+};
 
-function statics_handler (instance) {
-	response_file();
-	const f = instance.route;
-	instance.route = async function (req, res) {
+const statics = (instance) => {
+	use(instance, responseFile);
+	const staticsHandler = async (req, res, next) => {
 		const path = req.path;
 		if (req.method == 'GET' && path.startsWith('/statics')) {
 			return res.file(path.slice(1));
-		} else {
-			return f.call(this, req, res);
 		}
+		await next();
 	}
-}
-
-function register (name, callback) {
-	if (!(name in Extensions)) {
-		Extensions[name] = callback;
-	} else {
-		throw new NestError(`extension "${name}" is already registered`);
-	}
-}
-
-function use (instance, ...extensions) {
-	extensions.map(e => {
-		const extension = Extensions[e];
-		if (extension) {
-			extension(instance);
-		} else {
-			throw new NestError(`extension "${e}" is not available`);
-		}
-	});
-}
+	instance.use(staticsHandler);
+};
 
 module.exports = {
-	register,
+	apifs,
+	requestGet,
+	requestId,
+	requestValidate,
+	responseFile,
+	responseHtml,
+	responseRender,
+	statics,
 	use
 };
