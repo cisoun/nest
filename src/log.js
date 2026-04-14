c9323c36311fbb5f4e449889e4cff0ee40d28bc4
Cyriaque Skrapits <cyriaque.skrapits@swisscom.com>
2026-04-14 23:31:59 +0200

    Log: make colors opt-in

--- src/log.js
+++ src/log.js
@@ -5,8 +5,22 @@
 
 const { inspect } = require('node:util');
 
+/**
+ * Colors activation flag based on environment variables.
+ * Assume it's opt-in.
+ *
+ * @see https://nodejs.org/api/cli.html#no_colorany
+ * @see https://nodejs.org/api/cli.html#force_color1-2-3
+ * @see https://nodejs.org/api/cli.html#node_disable_colors1
+ */
+const USE_COLORS = (
+		['true', '1', '2', '3'].includes(process.env.FORCE_COLOR) ||
+		['false', '0'].includes(process.env.NODE_DISABLE_COLORS) ||
+		['false', '0'].includes(process.env.NO_COLOR)
+	);
+
 const object = (o, callback = log) => {
-	callback(inspect(o, {depth: null, colors: true, compact: false}));
+	callback(inspect(o, { depth: null, colors: false || USE_COLORS, compact: false }));
 }
 
 const format = (tag, callback, ...args) => {
